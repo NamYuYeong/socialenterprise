692b203a0afe645798817a12d3c0ebeea87cdd6f
NamYuYeong <42565054+NamYuYeong@users.noreply.github.com>
2018-11-24 17:44:54 +0900

    Add files via upload

--- js/quick.js
+++ js/quick.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
  /* quick menu */
- $(".quick").animate( { "top": $(document).scrollTop(0) + 0 +"px" }, 500 ); // 빼도 된다.
+ $(".quick").animate( { "top": $(document).scrollTop() + 0 +"px" }, 500 ); // 빼도 된다.
  $(window).scroll(function(){
   $(".quick").stop();
   $(".quick").animate( { "top": $(document).scrollTop() + 0 + "px" }, 1000 );
      
  });
+    
+});
+$(function(){
+	// 2단계 메뉴 닫기
+	$('.depth2').hide();
+	
+	// 메뉴 열고 닫기(hover)
+	$('.depth1 > li').hover(
+		function(){
+			$('.depth2')
+				.stop()
+				.slideDown(200);
+		},
+		function(){
+			$('.depth2')
+				.stop()
+				.slideUp(200);
+		}
+	);
 });
--- js/slide.js
+++ js/slide.js
@@ -13,4 +13,19 @@
                 pause: 2000 //재생간격(권장값 5~10초)
                 
             });
+       $('.depth2').hide();
+	
+	// 메뉴 열고 닫기(hover)
+	$('.depth1 > li').hover(
+		function(){
+			$('.depth2')
+				.stop()
+				.slideDown(200);
+		},
+		function(){
+			$('.depth2')
+				.stop()
+				.slideUp(200);
+		}
+	);
         });
